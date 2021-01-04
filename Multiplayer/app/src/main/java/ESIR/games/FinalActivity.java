924b00a98166cd9673198532d92573fe646003ba
EloyMartinez <eloyalbmartinez@gmail.com>
2021-01-04 16:41:18 +0100

    multiplayer is done

--- Multiplayer/app/src/main/java/ESIR/games/FinalActivity.java
+++ Multiplayer/app/src/main/java/ESIR/games/FinalActivity.java
@@ -7,17 +7,24 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FinalActivity extends AppCompatActivity {
 
     TextView score1, score2;
     FirebaseDatabase database;
     DatabaseReference messageRef;
+    DatabaseReference oppRef;
     String playerName = "";
     String roomName = "";
     String role = "";
+    String comp = "";
+    String scoreopp= "";
+
     String message = "";
 
 
@@ -36,13 +43,16 @@ public class FinalActivity extends AppCompatActivity {
         playerName = preferences.getString("playerName", "");
 
         score1.setText("hello");
-        preferences.edit().remove("roomName").apply(); // On enlève le nom de l'utilisateur de la memoire de l'apareil
+        preferences.edit().remove("roomName").apply();
 
 
             if (roomName.equals(playerName)) {
                 role = "player1";
+                comp="player2";
+
             } else {
                 role = "player2";
+                comp="player1";
             }
 
 
@@ -50,9 +60,29 @@ public class FinalActivity extends AppCompatActivity {
 
 
         messageRef = database.getReference("rooms/" + roomName + "/"+role+"score");
+         oppRef = database.getReference("rooms/" + roomName + "/"+comp+"score");
+
+
+        oppRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                System.out.println("TEST");
+                if(snapshot.exists()) {
+                    System.out.println("from snapshot"+snapshot.getValue());
+                    scoreopp = snapshot.getValue().toString();
+                    System.out.println("inside if" +scoreopp);
+                    score2.setText(scoreopp);
+                }
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }});
+
+
         messageRef.setValue(Integer.toString(getIntent().getIntExtra("score1", 0)));
 
+        score2.setText("waiting for oponent");
        score1.setText(Integer.toString(getIntent().getIntExtra("score1", 0)));
-       score2.setText(Integer.toString(getIntent().getIntExtra("score2", 0)));
     }
 }
