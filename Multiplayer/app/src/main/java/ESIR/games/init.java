3b5e9b6998aa2d2cd7337db31bb552c9d6c40b89
EloyMartinez <eloyalbmartinez@gmail.com>
2021-01-10 16:54:42 +0100

    things

--- Multiplayer/app/src/main/java/ESIR/games/init.java
+++ Multiplayer/app/src/main/java/ESIR/games/init.java
@@ -3,12 +3,16 @@ package ESIR.games;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import ESIR.games.Multiplayer.MultiSetUp;
 
@@ -20,12 +24,25 @@ public class init extends AppCompatActivity {
         setContentView(R.layout.activity_init);
         TextView text = findViewById(R.id.start);
         Button but = findViewById(R.id.button);
+        String delete;
 
         Button trainingBtn = findViewById(R.id.trainingBtn);
         trainingBtn.setOnClickListener(v -> {
             Intent intent = new Intent(this, TrainingActivity.class);
             startActivity(intent);
         });
+
+        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+
+        delete = preferences.getString("pastRoomName", "");
+        System.out.println("HERE WE ARE "+delete);
+        if (!delete.equals("")) {
+            FirebaseDatabase database;
+            database = FirebaseDatabase.getInstance();
+            DatabaseReference roomref = database.getReference("rooms/" + delete);
+            roomref.removeValue();
+           // preferences.edit().remove("delete").apply(); // On enlève le nom de l'utilisateur de la memoire de l'apareil
+        }
 
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/FredokaOneRegular.ttf");
         text.setTypeface(typeface);
